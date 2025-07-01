25f541e4109b51b346d5d74f9911eb111aa681b6
Divyansh Diwakar <ddivyanshdiwakar04@gmail.com>
2025-07-01 17:45:11 +0530

    Added req dev depedencies

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+// .prettierrc.js - Prettier configuration with comments as json file doesn't support comments that's why js file is created
+module.exports = {
+    semi: true, // Add semicolons at the ends of statements
+    singleQuote: true, // Use single quotes instead of double quotes
+    tabWidth: 2, // Set the number of spaces per indentation level
+    trailingComma: 'es5', // Add trailing commas where valid in ES5 (objects, arrays, etc.)
+    bracketSpacing: true // Print spaces between brackets in object literals
+};
